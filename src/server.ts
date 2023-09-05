18a9c34c4af78d49bca4b9ef3c90379dfd8e5300
Madhan Kanagavel <madhan@codelathe.com>
2023-09-04 20:17:24 -0500

    fixes

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,49 @@
 import errorHandler from "errorhandler";
 import app from "./app";
 
+import https from "https";
+import fs from "fs";
+import path from "path";
+
 /**
  * Error Handler. Provides full stack
  */
 if (process.env.NODE_ENV === "development") {
     app.use(errorHandler());
+
+
+    const server = app.listen(app.get("port"), () => {
+        console.log(
+            "  App is running at http://localhost:%d in %s mode",
+            app.get("port"),
+            app.get("env")
+        );
+        console.log("  Press CTRL-C to stop\n");
+    });
+    
+    
 }
+else
+{
+    console.log("Setting up SSL server");
+    const httpsOptions = {
+        cert: fs.readFileSync(path.join(__dirname, "env") + "/site.crt"),
+        ca: fs.readFileSync(path.join(__dirname, "env") + "/site.ca"),
+        key: fs.readFileSync(path.join(__dirname, "env") + "/site.key")
+    };
 
-/**
- * Start Express server.
- */
-const server = app.listen(app.get("port"), () => {
-    console.log(
-        "  App is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
-    );
-    console.log("  Press CTRL-C to stop\n");
-});
+    const server = https.createServer(httpsOptions, app);
 
-export default server;
+    /**
+     * Start Express server.
+     */
+    server.listen(app.get("port"), () => {
+        console.log(
+            "  App is running at https://localhost:%d in %s mode",
+            app.get("port"),
+            app.get("env")
+        );
+        console.log("  Press CTRL-C to stop\n");
+    });
+}
+//export default server;
