1c048d50aa6d7ae76f26786572f8321d9f43d8ae
Madhan Kanagavel <madhan@codelathe.com>
2023-09-04 19:23:10 -0500

    fixes

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,4 +1,4 @@
 import * as shell from "shelljs";
 
 shell.cp("-R", "src/public/*", "dist/public/");
-shell.cp("-R", "src/public/images", "dist/public/");
+//shell.cp("-R", "src/public/images", "dist/public/");
